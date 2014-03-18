237c990bc1d96b2206b94f727677b172e824dc24
Karol Tarasiuk <karoltarasiuk@gmail.com>
2014-03-18 20:23:08 +1100

    helper functions to modify links

--- public/wordpress/wp-content/themes/wp-json/functions.php
+++ public/wordpress/wp-content/themes/wp-json/functions.php
@@ -2,3 +2,11 @@
 /**
  * WP-JSON functions and definitions
  */
+
+function removeBeginningSlash($str) {
+	return ltrim($str, '/');
+}
+
+function removeTrailingSlash($str) {
+	return rtrim($str, '/');
+}
