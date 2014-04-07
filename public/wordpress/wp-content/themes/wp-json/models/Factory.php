d55c777160e4bbc538d00bc68d6225a94129bb93
Karol Tarasiuk <karoltarasiuk@gmail.com>
2014-04-07 21:13:36 +1000

    pages added

--- public/wordpress/wp-content/themes/wp-json/models/Factory.php
+++ public/wordpress/wp-content/themes/wp-json/models/Factory.php
@@ -11,6 +11,8 @@ class Factory {
     		case 'posts': $class = 'Posts'; break;
     		case 'get_categories':
     		case 'categories': $class = 'Categories'; break;
+    		case 'get_pages':
+    		case 'pages': $class = 'Pages'; break;
     	}
 
         if (empty($class) || !class_exists($class)) {
---
+++ public/wordpress/wp-content/themes/wp-json/models/Pages.php
@@ -0,0 +1,24 @@
+<?php
+
+class Pages extends AFunction {
+	
+	protected static $defaultArgs = array(
+		'sort_order' => 'ASC',
+		'sort_column' => 'post_title',
+		'hierarchical' => 1,
+		'exclude' => '',
+		'include' => '',
+		'meta_key' => '',
+		'meta_value' => '',
+		'authors' => '',
+		'child_of' => 0,
+		'parent' => -1,
+		'exclude_tree' => '',
+		'number' => '',
+		'offset' => 0,
+		'post_type' => 'page',
+		'post_status' => 'publish'
+	);
+	
+	protected static $functionName = 'get_pages';
+}
